0b22fea56d059f184d5461e270971cff0b40a31b
Anthony Candaele <info@anthonycandaele.com>
2025-02-11 10:19:23 +0100

    implement error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<main>
+	<div>
+		<h1>
+			{$page.error?.message || 'Something went wrong'}
+		</h1>
+		<p>
+			Sorry, {$page.status === 404
+				? 'we couldn’t find the page you’re looking for'
+				: 'the page you were looking for encountered an error'}.
+		</p>
+		<div>
+			<a href="/">
+				Go back home<span aria-hidden="true"> &rarr;</span>
+			</a>
+		</div>
+	</div>
+</main>
